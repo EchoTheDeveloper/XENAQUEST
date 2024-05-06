b48ce39494ffb9c34921041d007aa63ad0bb1970
dgenereal <158507822+dgenereal@users.noreply.github.com>
2024-05-06 11:27:39 +0100

    fancy css

---
+++ main.js
@@ -0,0 +1,10 @@
+function goToPeople() {
+    window.location.href = "people.html"
+}
+function goToDownloads() {
+    window.location.href = "downloads.html"
+}
+
+function mainPage() {
+    window.location.href = "../index.html"
+}
